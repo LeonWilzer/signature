bfcfd57368a4fc92b7caede3cdd08e93d265bce1
Leon Wilzer <leon.wilzer@protonmail.com>
2023-04-20 21:08:43 +0200

    mi replicator blacklist added; can't sign sigend items anymore

--- src/main/java/signature/command/SignStackCommand.java
+++ src/main/java/signature/command/SignStackCommand.java
@@ -14,6 +14,7 @@ import static net.minecraft.server.command.CommandManager.argument;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -21,45 +22,53 @@ import net.minecraft.util.Hand;
 
 public class SignStackCommand {
 
-        public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-                dispatcher.register(
-                                literal("signstack").executes(ctx -> signstack(ctx.getSource(), null))
-                                                .then(argument("description", greedyString())
-                                                                .executes(ctx -> signstack(ctx.getSource(),
-                                                                                getString(ctx, "description")))));
-        }
+		public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+				dispatcher.register(
+								literal("signstack").executes(ctx -> signstack(ctx.getSource(), null))
+												.then(argument("description", greedyString())
+																.executes(ctx -> signstack(ctx.getSource(),
+																				getString(ctx, "description")))));
+		}
 
-        public static int signstack(ServerCommandSource serverCommandSource, String description)
-                        throws CommandSyntaxException {
-                ServerPlayerEntity player = serverCommandSource.getPlayer();
-                ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
-                if (stack.isEmpty()) {
-                        serverCommandSource.sendError(Text.of("Please hold an item in your main hand."));
-                        return -1;
-                }
+		public static int signstack(ServerCommandSource serverCommandSource, String description)
+						throws CommandSyntaxException {
+				ServerPlayerEntity player = serverCommandSource.getPlayer();
+				ItemStack stack = player.getStackInHand(Hand.MAIN_HAND);
+				if (stack.isEmpty()) {
+					serverCommandSource.sendError(Text.of("Please hold an item in your main hand."));
+					return -1;
+				}
 
-                String nbtString;
+				NbtCompound nbt = stack.getNbt();
 
-                if (description == null) {
-                        nbtString = String.format(
-                                        "{display:{Lore:['[{\"text\":\"Signed by \",\"color\":\"blue\"},{\"text\":\"%s\",\"underlined\":true}]']}}",
-                                        player.getEntityName());
-                } else {
-                        description = description.replaceAll("\"", "\\\\\"");
-                        description = description.replaceAll("\"", "\\\\\"");
-                        description = description.replaceAll("'", "\\\\'");
+				if(nbt!=null && !nbt.isEmpty() && nbt.contains("isSigned") && nbt.getBoolean("isSigned"))
+				{
+					serverCommandSource.sendError(Text.of("This item is already signed."));
+					return -1;
+				}
 
-                        nbtString = String.format(
-                                        "{display:{Lore:['[{\"text\":\"%1$s\",\"italic\":false,\"color\":\"gold\"},{\"text\":\"\"}]','[{\"text\":\"\"}]','[{\"text\":\"Signed by \",\"italic\":true,\"color\":\"blue\"},{\"text\":\"%2$s\",\"underlined\":true}]']}}",
-                                        description, player.getEntityName());
+				String nbtString;
 
-                }
+				if (description == null) {
+						nbtString = String.format(
+										"{isSigned:1b,display:{Lore:['[{\"text\":\"Signed by \",\"color\":\"blue\"},{\"text\":\"%s\",\"underlined\":true}]']}}",
+										player.getEntityName());
+				} else {
+						description = description.replaceAll("\"", "\\\\\"");
+						description = description.replaceAll("\"", "\\\\\"");
+						description = description.replaceAll("'", "\\\\'");
 
-                NbtCompound nbt = NbtHelper.fromNbtProviderString(nbtString);
+						nbtString = String.format(
+										"{isSigned:1b,display:{Lore:['[{\"text\":\"%1$s\",\"italic\":false,\"color\":\"gold\"},{\"text\":\"\"}]','[{\"text\":\"\"}]','[{\"text\":\"Signed by \",\"italic\":true,\"color\":\"blue\"},{\"text\":\"%2$s\",\"underlined\":true}]']}}",
+										description, player.getEntityName());
 
-                stack.setNbt(nbt);
-                serverCommandSource.sendFeedback(
-                                Text.of("Item stack signed!"), false);
-                return Command.SINGLE_SUCCESS;
-        }
+				}
+
+				nbt = NbtHelper.fromNbtProviderString(nbtString);
+
+				stack.setNbt(nbt);
+				serverCommandSource.sendFeedback(
+								Text.of("Item stack signed!"), false);
+				return Command.SINGLE_SUCCESS;
+		}
 }
